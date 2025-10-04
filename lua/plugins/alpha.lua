d7fb7ee99bcea84a07fbda4cfb10068c98ef5d1d
chaseungjoon <cktmdwns3@gmail.com>
2025-10-05 01:03:58 +0900

    Update header color

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -25,6 +25,11 @@ return {
             [[                                                                       ]],
             [[                                                                       ]],
         }
+        -- Header Color
+        vim.api.nvim_set_hl(0, "AlphaHeader", { fg = "#FF6B00", bold = true })
+        dashboard.section.header.opts.hl = "AlphaHeader"
+
+        -- Buttons
         dashboard.section.buttons.val = {
             dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
             dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
@@ -33,6 +38,8 @@ return {
             dashboard.button("c", "  Configuration", ":cd ~/.config/nvim | Neotree toggle<CR>"),
             dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
         }
+
+        -- Footers
         local function get_current_time()
             return os.date("%Y/%m/%d %H:%M")
         end
@@ -48,7 +55,7 @@ return {
             return string.format("       %s  %s  macOS %s", cpu, arch, osver)
         end
 
-        local flines = {"", "", "", "","", get_user_host(), get_system_info(),"" , "              " .. get_current_time() }
+        local flines = {"", "","", "", "","", get_user_host(), get_system_info(),"" , "              " .. get_current_time() }
         dashboard.section.footer.val = flines
         dashboard.section.footer.opts.hl = "Comment"
 
